ad480597a40e62189ef72bf9cfcfc356d918cdbb
Plush-Jill <bockarevvlad23@gmail.com>
2025-02-15 09:31:22 +0700

    Написал вычисление, вроде готово.

--- lab-1/src/main.cpp
+++ lab-1/src/main.cpp
@@ -20,8 +20,14 @@ private:
     float m_hx;
     float m_hy;
 
+    float m_hx_hy_2;
+    float m_a;
+    float m_b;
+    float m_c;
+
+
     std::vector<HeatSource> m_heat_sources;
-    std::vector<std::vector<std::pair<float, float>>> m_grid;
+    std::vector<std::vector<std::pair<float, float>>> m_grid; //first - heat, second - function
 
 public:
     explicit PoissonEquationSolver(const std::string& config_file) :
@@ -67,27 +73,38 @@ public:
 
 
     void solve() {
-        float hx_hy_2 {std::pow(m_hx, -2) + std::pow(m_hy, -2)};
-        float a {0.2f * hx_hy_2};
-        float b {0.5f * (5 * std::pow(m_hx, -2) - std::pow(m_hy, -2))};
-        float c {0.25f * hx_hy_2};
+        m_hx_hy_2 = static_cast<float>(std::pow(m_hx, -2) + std::pow(m_hy, -2));
+        m_a = 0.2f * m_hx_hy_2;
+        m_b = 0.5f * static_cast<float>(5 * std::pow(m_hx, -2) - std::pow(m_hy, -2));
+        m_c = 0.25f * m_hx_hy_2;
 
-        auto function = [](float F_im1_jm1, float F_im1_j, float F_im1_jp1,
-                           float F_i_jm1,                  float F_i_jp1,
-                           float F_ip1_jm1, float F_ip1_j, float F_ip1_jp1,
+        auto function = [this](float F_im1_jm1, float F_im1_j, float F_im1_jp1,
+                               float F_i_jm1,                  float F_i_jp1,
+                               float F_ip1_jm1, float F_ip1_j, float F_ip1_jp1,
 
-                                            float P_im1_j,
-                           float P_i_jm1,   float P_i_j,   float P_i_jp1,
-                                            float P_ip1_j
-                                            ) {
+                                                float P_im1_j,
+                               float P_i_jm1,   float P_i_j,   float P_i_jp1,
+                                                float P_ip1_j
+        ) {
+            const float result = static_cast<float> (m_a * (m_b * (F_i_jm1 + F_i_jp1) + m_b * (F_im1_j + F_im1_j + F_ip1_j)
+                + m_c * (F_im1_jp1 + F_im1_jp1 + F_ip1_jm1 + F_ip1_jp1)
+                + 2 * P_i_j + 0.25 * (P_im1_j + P_ip1_j + P_i_jm1 + P_i_jp1) ));
 
-        }
+            return result;
+        };
 
         // F_i_jp1, F_i_jm1, F_ip1_jp1, F_ip1_j, F_ip1_jm1, F_im1_jp1, F_im1_j, F_im1_jm1
         for (int time {}; time < m_Nt; ++time) {
             for (int i {1}; i < m_Nx - 1; ++i) {
                 for (int j {1}; j < m_Ny - 1; ++j) {
+                    m_grid[i][j].first = function(m_grid[i-1][j-1].second, m_grid[i-1][j].second, m_grid[i-1][j+1].second,
+                                                  m_grid[i][j-1].second,                                     m_grid[i][j+1].second,
+                                                  m_grid[i+1][j-1].second,   m_grid[i+1][j].second, m_grid[i+1][j+1].second,
 
+                                                                             m_grid[i-1][j].second,
+                                                  m_grid[i][j-1].second, m_grid[i][j].second, m_grid[i][j+1].second,
+                                                                              m_grid[i+1][j].second
+                                                  );
                 }
             }
         }
