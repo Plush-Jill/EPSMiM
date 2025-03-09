ad8a99b329cc39a614101940779ad67342880058
Plush-Jill <bockarevvlad23@gmail.com>
2025-03-09 19:34:44 +0700

    added profile script

---
+++ lab-1/profile/profile-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+g++ ../src/main.cpp ../src/heat_source_circle.hpp ../src/heat_source_circle.cpp -funroll-loops -std=c++20 -lboost_json -pg -o ../cmake-build-debug/lab-1
+sudo perf record -F 99 -g -- ../cmake-build-debug/lab-1	
+sudo perf script > out.perf
+../../../FlameGraph/stackcollapse-perf.pl out.perf > out.folded
+../../../FlameGraph/flamegraph.pl out.folded  > flamegraph.svg
+xdg-open flamegraph.svg
